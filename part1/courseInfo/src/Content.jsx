9f96681f534346cb7c76eef87e666fb7d3250657
JessAm <jessamelie.sola@gmail.com>
2025-04-17 14:59:15 +0200

    part 1 / 1.2: Course Information, step 2

--- part1/courseInfo/src/Content.jsx
+++ part1/courseInfo/src/Content.jsx
@@ -1,9 +1,20 @@
-const Content = (props) => {
+export const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
   return (
     <>
-      {props.title} {props.exercises}
-      <br />
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
     </>
   );
 };
+
 export default Content;
+
