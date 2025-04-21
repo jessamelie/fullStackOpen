c6dceee66961367ad56500f87431fc9e944f0c50
JessAm <jessamelie.sola@gmail.com>
2025-04-21 23:22:04 +0200

    part 1 / 1.6: Unicafe step 1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import Title from './Title'
+import Button from './Button'
+import Statistics from './Statistics'
+
+const App = () => {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodFeedback = () => {
+    setGood((prev) => prev + 1)
+  }
+  const handleNeutralFeedback = () => {
+    setNeutral((prev) => prev + 1)
+  }
+  const handleBadFeedback = () => {
+    setBad((prev) => prev + 1)
+  }
+
+  return (
+    <>
+      <Title text={"give feedback"} />
+      <Button text={"good"} handleClick={handleGoodFeedback}/>
+      <Button text={"neutral"} handleClick={handleNeutralFeedback}/>
+      <Button text={"bad"} handleClick={handleBadFeedback}/>
+      <Title text={"statistics"} />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+
+    </>
+  )
+}
+
+export default App
---
+++ part1/unicafe/src/Statistics.jsx
@@ -0,0 +1,11 @@
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <>
+      <p> good {good} </p>
+      <p> neutral {neutral} </p>
+      <p> bad {bad} </p>
+    </>
+  );
+};
+
+export default Statistics;
