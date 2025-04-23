eebf0f589b9447cf79e1c1f3d1caa5ddec29fad6
JessAm <jessamelie.sola@gmail.com>
2025-04-23 19:41:50 +0200

    part 2 / 2.5 : Course Information

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -1,10 +1,32 @@
-import Header from "./Header";
-import Content from "./Content";
+const Header = ({ name }) => <h2>{name}</h2>;
 
-const Course = ({ course }) => (
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+      <p>Total of exercises {total}</p>
+    </>
+  );
+};
+
+const Course = ({ courses }) => (
   <>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
+    {courses.map((course) => (
+      <section key={course.id}>
+        <Header name={course.name} />
+        <Content parts={course.parts} />
+      </section>
+    ))}
   </>
 );
 
